11eddb9b8b71000a9158c1d4608f4eccf9af6c74
idolieb26 <idolieb26@gmail.com>
2017-10-28 23:30:16 -0600

    Setup Redux and Redux-Saga

--- src/reducers/exchange.js
+++ src/reducers/exchange.js
@@ -1,9 +1,14 @@
+import { REQUEST_API_DATA, RECEIVE_API_DATA } from "../actions/actions";
 const defaultState = {};
 
-export default (state = defaultState, action) => {
+export default (state = defaultState, action, data = "") => {
   switch (action.type) {
-    case "UPDATE_ETH":
-      return { ...state };
+    case REQUEST_API_DATA:
+      return "Requsted data";
+    // return { ...state };
+    case RECEIVE_API_DATA:
+      // return { ...state };
+      return data;
     default:
       return state;
   }
--- src/store.js
+++ src/store.js
@@ -1,18 +1,28 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+
 import rootReducer from "./reducers";
+import saga from "./sagas";
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const initialState = {
   exchange: {
-    dash: 0,
-    eth: 0,
-    ltc: 0
+    ex1: {
+      dash: 0,
+      eth: 0,
+      ltc: 0
+    }
   }
 };
 
 const store = createStore(
   rootReducer,
   initialState,
+  applyMiddleware(sagaMiddleware),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+sagaMiddleware.run(saga);
 
 export default store;
